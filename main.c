c4d28edd19901fab98ac14f85754ae2cd7bc9f68
thomas englert <thomas@englert.one>
2020-02-19 19:23:58 +0100

    implemented help page function

--- main.c
+++ main.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int checkFirstChar;
 int usedOption;
+int choosedOption;
 
-
-void validateUserInput(int firstInputChar, int secondInputChar){
+int validateUserInput(int firstInputChar, int secondInputChar){
     if (firstInputChar == 45 && secondInputChar == 104){
-        printf("\n\nSie haben die Hilfe gewählt! \n");
+        return secondInputChar;
     } else if (firstInputChar == 45 && secondInputChar == 98){
-        printf("\n\nSie haben die Großbuchstaben-Variante gewählt! \n");
+        return secondInputChar;
     } else if (firstInputChar == 45 && secondInputChar == 115){
-        printf("\n\nSie haben die Kleinbuchstaben-Variante gewählt! \n");
+        return secondInputChar;
     } else if (firstInputChar == 45 && secondInputChar == 119){
-        printf("\n\nSie haben die word-Variante gewählt! \n");
+        return secondInputChar;
     } else {
         printf("\nUsage: ./main.c {-h for help|-b|-s|-c \"word\"}\n");
+        exit(1);
     }
+}
+void printHelp(){
+    printf("\n====================\t HELPPAGE \t==================\n");
+    printf("\nUsage: ./main.c [Options] \n");
+    printf("OPTIONS:\n");
+    printf("-b: count all upper cases in the choosed file.\n");
+    printf("-s: count all lowercases in the choosed file. \n");
+    printf("-c <word>:  check if word is in the file.\n");
+    printf("\n====================\t HELPPAGE \t==================\n");
+    exit(1);
+}
+
+int countUpperCases(){
+
+}
+
+int countLowerCases(){
+
+}
+
+int wordIsInFile(){
+
 }
 
 int main(int argc, char *argv[]) {
 
     checkFirstChar = argv[1][0];
-
-    printf("FIRST LETTER %d\n\n", checkFirstChar);
-
     usedOption = argv[1][1];
-    validateUserInput(checkFirstChar, usedOption);
 
     FILE *file = fopen("/home/thomas/CLionProjects/Uebung_14_02_2020/characters.txt", "r");
     if (file == NULL){
@@ -33,6 +53,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    choosedOption = validateUserInput(checkFirstChar, usedOption);
+
+    if (choosedOption == 104){
+        printHelp();
+    } else if (choosedOption == 98){
+    } else if (choosedOption == 115){
+    } else if (choosedOption == 119){
+
+    }
+
+    printf("choosed option %d", choosedOption);
     char characterInFile;
     int characterCounter = 0;
     int spaceAndReturnCounter = 0;
